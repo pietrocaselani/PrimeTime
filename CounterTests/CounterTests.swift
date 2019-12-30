9a3fb41d411d71193bf2e121e985e66bf3710b70
Pietro Caselani <pietro.caselani@getyourguide.com>
2019-12-30 10:56:19 +0100

    Fix testNthPrimeButtonHappyFlow

--- CounterTests/CounterTests.swift
+++ CounterTests/CounterTests.swift
@@ -4,12 +4,13 @@ import ArchitectureTestable
 
 final class CounterTests: XCTestCase {
   func testIncrDecrButtonTapped() {
-    assert(initialValue: CounterViewState(),
-           reducer: counterViewReducer,
-           steps:
+    assert(
+      initialValue: CounterViewState(),
+      reducer: counterViewReducer,
+      steps:
       Step(.send, .counter(.incrTapped)) { $0.count = 1 },
-           Step(.send, .counter(.incrTapped)) { $0.count = 2 },
-           Step(.send, .counter(.decrTapped)) { $0.count = 1 }
+      Step(.send, .counter(.incrTapped)) { $0.count = 2 },
+      Step(.send, .counter(.decrTapped)) { $0.count = 1 }
     )
   }
 
@@ -26,11 +27,11 @@ final class CounterTests: XCTestCase {
       Step(.send, .counter(.nthPrimeButtonTapped)) {
         $0.isNthPrimeButtonDisabled = true
       },
-      Step(.receive, .counter(.nthPrimeResponse(17))) {
-        $0.alertNthPrime = PrimeAlert(prime: 17)
+      Step(.receive, .counter(.nthPrimeResponse(Result.success(17)))) {
+        $0.alertNthPrime = PrimeAlert(title: "Prime", message: "The 0th prime is 17")
         $0.isNthPrimeButtonDisabled = false
       },
-      Step(.send, .counter(.alertDismissButtonTapped)) {
+      Step(.send, .counter(.nthPrimeAlertDismissed)) {
         $0.alertNthPrime = nil
       }
     )
